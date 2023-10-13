65851b2b3cdf3cf9083d4c439396d7ccad961804
ndoye Philip Ndula <ndoyephilipndula@gmail.com>
2023-10-13 03:45:51 +0100

    new stuff

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const mongoose = require("mongoose")
 const app = express()
+
+const {MongoClient, ServerApiVersion} = require("mongodb")
+
+
+const Uri = 'mongodb://localhost:27017/project?directConnection=true';
+
+
+
+mongoose.connect(Uri)
 
 app.use(bodyParser.json())
 
 app.get("/", (req, res, next)=>{
     res.send({message: "Hello Mom"})
+
+
 })
 
 app.use((req, res)=>{
