5aafa425c48bb60faeb0f5d934330220139f7fc5
ndoye Philip Ndula <ndoyephilipndula@gmail.com>
2023-10-14 04:15:31 +0100

    adding production

--- index.js
+++ index.js
@@ -10,7 +10,64 @@ const Uri = 'mongodb://localhost:27017/project?directConnection=true';
 
 
 
-mongoose.connect(Uri)
+function rotate(matrix, direction) {
+  
+    if(matrix.length <= 1){
+      return matrix;
+    }
+    
+    var result = [];
+
+    const rowCount = matrix.length, colCount = matrix[0].length;
+
+    console.log("Row:", rowCount)
+    console.log("Col:", colCount)
+    // console.log("Array:", matrix)
+    switch(direction){
+        
+        case "clockwise":
+
+        var insider = [];
+        for(let j = 0; j < colCount; j++){
+            insider[j] = [];
+            insider[j].length = rowCount 
+        }
+        console.log("Insider", insider)
+
+        for(let i = 0; i < colCount; i++){
+            for(let j = 0; j < rowCount; j++){
+                console.log("i: " + i + ",  j:" + j)
+
+                insider[j][i] = matrix[j][i]
+            }
+    
+        }
+
+
+
+        console.log("Insider", insider)
+
+        
+            
+        break;
+  
+        
+        break;
+        
+        default:
+        break;
+    }
+    
+    return result;
+  }
+
+  console.log("Result:", rotate([[3,6,9,12],[2,5,8,11],[1,4,7,10]], 'clockwise'))
+
+
+
+
+
+// mongoose.connect(Uri)
 
 app.use(bodyParser.json())
 
